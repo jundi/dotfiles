11f622f34e18df0cb699b598e311f00855060447
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2020-01-31 15:50:54 +0200

    Disable searches in FF urlbar

--- user.js
+++ user.js
@@ -1,3 +1,6 @@
 user_pref("browser.fullscreen.autohide", false);
 user_pref("browser.urlbar.maxRichResults", 30);
+user_pref("browser.urlbar.trimURLs", false);
+user_pref("browser.urlbar.suggest.openpage", false);
+user_pref("browser.urlbar.suggest.searches", false);
 user_pref("browser.fixup.alternate.prefix", "");
