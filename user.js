b0d6568fdf119ebb003025640d414396bf8ff82c
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2020-04-28 14:39:52 +0300

    Firefox default preferences

--- user.js
+++ user.js
@@ -1,6 +1,16 @@
 user_pref("browser.fullscreen.autohide", false);
 user_pref("browser.urlbar.maxRichResults", 30);
 user_pref("browser.urlbar.trimURLs", false);
+user_pref("browser.search.suggest.enabled", false);
 user_pref("browser.urlbar.suggest.openpage", false);
 user_pref("browser.urlbar.suggest.searches", false);
 user_pref("browser.fixup.alternate.prefix", "");
+user_pref("general.smoothScroll", false);
+user_pref("accessibility.force_disabled", 1);
+user_pref("browser.startup.homepage", "about:blank");
+user_pref("browser.urlbar.placeholderName", "DuckDuckGo");
+user_pref("browser.contentblocking.category", "strict");
+user_pref("browser.download.dir", "~/downloads");
+user_pref("browser.ctrlTab.recentlyUsedOrder", false);
+user_pref("browser.shell.checkDefaultBrowser", true);
+user_pref("privacy.donottrackheader.enabled", true);
