847c71ed9fc688834a518effbb29c81af61664cf
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2022-12-09 11:34:51 +0200

    Support old notify-send

--- dotfiles/opt/scripts/showclipboard.sh
+++ dotfiles/opt/scripts/showclipboard.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-notify-send -t 2000 -a "clipboard" "`wl-paste`" -r 1145867
-notify-send -t 2000 -a "primary" "`wl-paste -p`" -r 9675876
+if [[ `notify-send --version` == 'notify-send 0.7.9' ]]; then
+	notify-send -t 2000 "clipboard: `wl-paste`"
+	notify-send -t 2000 "primary: `wl-paste -p`"
+else
+	notify-send -t 2000 -a "clipboard" "`wl-paste`" -r 1145867
+	notify-send -t 2000 -a "primary" "`wl-paste -p`" -r 9675876
+fi
